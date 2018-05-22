e65996f708065783aa2c56aee4262cb260035924
John <crawfo58@purdue.edu>
2018-05-22 14:12:32 -0400

    Style NoteList and NoteForm by importing CSS

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -1,11 +1,47 @@
 import React from 'react'
 
+import './NoteForm.css'
+
 const NoteForm = () => {
   return (
     <div className="NoteForm">
-      NoteForm
-    </div>
+          <div className="form-actions">
+            <button type="button">
+              <i className="fa fa-trash-o"></i>
+            </button>
+          </div>
+          <form>
+            <p>
+              <input
+                type="text"
+                name="title"
+                placeholder="Title your note"
+              />
+            </p>
+            
+            <textarea name="body"></textarea>
+          </form>
+        </div>
   )
 }
+
+// const styles = {
+//   noteForm: {
+//     flexPositive: '1',
+//     flexGrow: '1',
+//     padding: '0 3rem'
+//   },
+//   formActions: {
+//     paddingTop: '1rem',
+//     marginLeft: '-2rem',
+//     display: '-ms-flexbox',
+//     display: 'flex',
+//     msFlexLinePack: 'center',
+//     alignContent: 'center'
+//   },
+//   button: {
+
+//   },
+// }
 
 export default NoteForm
