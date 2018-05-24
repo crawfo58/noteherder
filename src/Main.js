8450cacee06305d3795680158f4e57633145a7de
John <crawfo58@purdue.edu>
2018-05-24 10:04:47 -0400

    Store notes in localStorage

--- src/Main.js
+++ src/Main.js
@@ -13,6 +13,13 @@ class Main extends React.Component {
     }
   }
 
+  componentDidMount() {
+    const notes = JSON.parse(window.localStorage.getItem('notes'))
+    if(notes) {
+      this.setState({notes})
+    }
+  }
+
   blankNote = () => {
     return {
       id: null,
@@ -43,6 +50,8 @@ class Main extends React.Component {
 
     this.setState({notes})
     this.setCurrentNote(note)
+
+    window.localStorage.setItem('notes', JSON.stringify(notes))
   }
 
   deleteNote = (note) => {
@@ -51,6 +60,7 @@ class Main extends React.Component {
     if(i > -1) {
       notes.splice(i,1)
       this.setState({notes})
+      window.localStorage.setItem('notes', JSON.stringify(notes))
     } 
     this.resetCurrentNote(note)
   }
