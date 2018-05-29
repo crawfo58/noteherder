2e99cd1d910658435727dba4fc8be11e8c5ef75e
John <crawfo58@purdue.edu>
2018-05-29 09:19:51 -0400

    Scope notes by user

--- src/Main.js
+++ src/Main.js
@@ -15,7 +15,7 @@ class Main extends React.Component {
   }
 
   componentDidMount() {
-    base.syncState('notes', {
+    base.syncState(`${this.props.id}`, {
       context: this,
       state: 'notes',
       asArray: true
